28400fcbfdaa9c884717a1aa89d5dd21621d62fd
camcoder337 <camcoder337@gmail.com>
2025-07-06 22:28:11 +0100

    refactore: use dev env by default

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    # Utiliser development par défaut
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
